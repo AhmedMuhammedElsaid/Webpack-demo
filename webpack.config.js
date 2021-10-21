a3253c9f9cebcba21b2008710e5da04ad324ab8e
Ahmed Said <ahmedelsaid@techytypes.com>
2021-10-21 13:54:19 +0200

    add html-webpack-plugin and implement it into our webpack config file .

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
  const path = require("path");
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
    mode: "development",
    entry:"./src/index.js",
    output: {
      path: path.resolve(__dirname, "dist"),
-      filename: "main.js",
-    //   filename: "main.[contentHash].js", // in order to use content hash naming pattern
+    //   filename: "main.js",
+      filename: "main.[contentHash].js", // in order to use content hash naming pattern
    },
+   plugins:[
+       new HtmlWebpackPlugin({
+           template:"./src/template.html"
+       })
+   ],
    module: {
      rules: [
        {
